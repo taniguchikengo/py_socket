e29a4836971d75590d660e4c38f57c00a987f27c
taniguchikengo <76574238+taniguchikengo@users.noreply.github.com>
2025-03-11 10:45:09 +0900

    Add files via upload

--- client5_2.py
+++ client5_2.py
@@ -12,6 +12,7 @@ import tkinter.ttk as ttk
 PORT = 50000
 BUF = 4096
 client = socket(AF_INET, SOCK_DGRAM)
+n = 0
 
 def main():
     root = Tk()
@@ -37,40 +38,42 @@ def main():
     en1.insert(0, "192.168.10.21")
     en2 = Entry(root, width=100)
     lab2 = Label(root,text="送信メッセージ記入欄")
-    # メッセージ送信ボタン
-    bt1 = Button(root, text="接続", width=10, command=lambda: server_connect(en1.get(),tree1))
-    bt2 = Button(root,text="送信",width=10,command=lambda :send_msg(en1.get(),en2))
+    bt1 = Button(root,text="送信",width=10,command=lambda :send_msg(en1.get(),en2))
 
     tree1.place(x=20, y=10)
     lab1.place(x=20,y=378)
     en1.place(x=20, y=400)
-    bt1.place(x=250,y=395)
+
     lab2.place(x=20, y=425)
     en2.place(x=20, y=450)
-    bt2.place(x=650, y=445)
+    bt1.place(x=650, y=445)
 
-#サーバー接続とスレッド作成
-def server_connect(host,tree1):
-    p = threading.Thread(target=server_h, args=(client,tree1))
+    p = threading.Thread(target=server_h, args=(client, tree1))
     p.setDaemon(True)
+    p.start()
 
 def send_msg(host,en):
     msg =en.get()
     client.sendto(msg.encode("UTF-8"), (host, PORT))
     en.delete("0",END)
+    if msg=="0":
+        client.close()
+        print("クローズ")
+
 
 def server_h(client,tree1):
-    n = 0
+    global n
     while True:
-        n += 1
         try:
-            data = client.recv(BUF)
+            data,addr = client.recvfrom(BUF)
             ti = datetime.datetime.now().strftime("%H:%M:%S")
-            tree1.insert(parent="", index="end", iid=n, values=(ti, client[0], data.decode('UTF-8')))
+            tree1.insert(parent="", index="end", iid=n, values=(ti, addr[0], data.decode('UTF-8')))
             print(data.decode("UTF-8"))
+            n += 1
         except:
             sys.exit()
-    #client.close()
+
+
 
 main()
 mainloop()
